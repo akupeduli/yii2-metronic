e229cf8e0a81d4490fcd531434d133099caba07f
L Shaf <shafry@mimicreative.net>
2017-10-12 17:33:04 +0700

    add default params

--- src/config/params.php
+++ src/config/params.php
@@ -1,3 +1,9 @@
 <?php
+use yii\helpers\Html;
 
-return [];
+return [
+    'metronic' => [
+        'footer' => 'Yii2 Metronic by ' . Html::a('L Shaf', 'https://github.com/lshaf', ['target' => '_blank']),
+        'copyright' => 'Yii2 Metronic Admin Integration by ' . Html::a('L Shaf', 'https://github.com/lshaf', ['target' => '_blank'])
+    ],
+];
